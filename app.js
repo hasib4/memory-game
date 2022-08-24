76b52141f41a685e7be2c3adfe04ba85b4669ff3
hasib4 <hasib4noor@gmail.com>
2022-08-24 12:36:35 +0600

    Game board configured

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'img/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'img/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'img/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'img/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'img/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'img/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'img/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'img/pizza.png',
+    },
+];
 
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.getElementById('grid');
+const cardsChosen = [];
+
+const createBoard = () => {
+    cardArray.forEach(ele = (obj,i) => {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'img/blank.png');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click',flipCard);
+        gridDisplay.append(card);
+    })
+};
+
+createBoard();
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id');
+    // console.log('clicked', cardId)
+    cardsChosen.push(cardArray[cardId].name);
+    this.setAttribute('src', cardArray[cardId].img);
+}
